c5e7ae3e6d65fe742851a5e2e1931582b335fd8d
lily <lilyliang0021@gmail.com>
2023-07-21 13:44:01 +0000

    update data.db and pm2 settings

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps : [{
+    name: 'strapi-dev',
+    cwd: '/home/lily/chat-backend',
+    script: 'npm',
+    args: 'start',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
